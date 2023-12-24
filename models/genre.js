1e8ae2a743185dd9762b2285407603e776dcbbec
Hans Ganter <hansi.ganter@gmx.at>
2023-12-24 11:37:01 +0100

    finish Express Tutorial Part 5: Displaying library data

--- models/genre.js
+++ models/genre.js
@@ -3,9 +3,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-    name: { type: String, required: true, maxLength: 100 }
+  name: { type: String, required: true, maxLength: 100 },
 });
 
-GenreSchema.virtual("url").get(() => `/catalog/author/${this._id}`);
+// Virtual for genre's URL
+GenreSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/genre/${this._id}`;
+});
 
 module.exports = mongoose.model("Genre", GenreSchema);
